7a1ed306889af70a1014e0efee72d0b86a9bd8d4
cja22 <cja22@LAPTOP-MCNK8S6Q>
2018-08-22 13:40:17 +0800

    重新配置

--- src/cn/appmanager/service/dev_user/Dev_userService/Dev_userService.java
+++ src/cn/appmanager/service/dev_user/Dev_userService/Dev_userService.java
@@ -1,5 +1,8 @@
 package cn.appmanager.service.dev_user.Dev_userService;
 
-public interface Dev_userService {
+import cn.appmanager.pojo.Div_User;
 
+public interface Dev_userService {
+	
+	public int userlogin(Div_User div_user);
 }
--- src/cn/appmanager/service/dev_user/Dev_userService/Dev_userServiceImpl.java
+++ src/cn/appmanager/service/dev_user/Dev_userService/Dev_userServiceImpl.java
@@ -1,8 +1,21 @@
 package cn.appmanager.service.dev_user.Dev_userService;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
+
+import cn.appmanager.dao.div_user.Div_UserMapper;
+import cn.appmanager.pojo.Div_User;
 
 @Service
 public class Dev_userServiceImpl implements Dev_userService{
-
+	@Resource
+	private Div_UserMapper  div_UserMapper;
+	
+	@Override
+	public int userlogin(Div_User div_user) {
+		// TODO Auto-generated method stub
+		return div_UserMapper.div_UserLogin(div_user.getDevCode(), div_user.getDevPassword());
+	}
+	
 }
