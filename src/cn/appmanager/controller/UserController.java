f8d9db4d65ea0ca5ee0d0c27ac1e5c8740a07a19
acer <acer@acer-PC>
2018-08-22 14:29:54 +0800

    ss

--- src/cn/appmanager/controller/UserController.java
+++ src/cn/appmanager/controller/UserController.java
@@ -1,15 +1,38 @@
 package cn.appmanager.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import cn.appmanager.pojo.App_Info;
+import cn.appmanager.service.app_info.App_infoService;
 
 @Controller
 @RequestMapping(value="/user")
 public class UserController {
+	@Resource
+	private App_infoService app_infoService;
 	
 	@RequestMapping(value="/test.html")
 	public String test(){
 		System.out.println(">>>>>>>>>>>>>>>>>>>>>.");
 		return "test";
 	}
+	//修改APP基本信息
+	@RequestMapping(value="/updateApp_Info.html")
+	public String updateApp_Info(){
+		return "updateApp_Info";
+	}
+	
+	@RequestMapping(value="/updateApp_InfoSave.html",method=RequestMethod.POST)
+	public String updateApp_InfoSave(@RequestParam("App_Info")App_Info app_Info){
+		if()
+		return "a";
+	}
+
+	
+	
 }
--- src/cn/appmanager/service/app_info/App_infoServiceImpl.java
+++ src/cn/appmanager/service/app_info/App_infoServiceImpl.java
@@ -1,8 +1,13 @@
 package cn.appmanager.service.app_info;
 
+import javax.annotation.Resource;
+
 import org.springframework.stereotype.Service;
 
-@Service
-public interface App_infoServiceImpl extends App_infoService {
+import cn.appmanager.dao.app_info.App_InfoMapper;
 
+@Service
+public class App_infoServiceImpl implements App_infoService {
+	@Resource
+	private App_InfoMapper app_InfoMapper;
 }
