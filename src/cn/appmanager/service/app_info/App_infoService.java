f486acf44d943269513b7134196c95bc738360eb
acer <acer@acer-PC>
2018-08-23 09:34:38 +0800

    查信息Byid

--- src/cn/appmanager/service/app_info/App_infoService.java
+++ src/cn/appmanager/service/app_info/App_infoService.java
@@ -1,5 +1,13 @@
 package cn.appmanager.service.app_info;
 
-public interface App_infoService {
 
+import org.apache.ibatis.annotations.Param;
+
+import cn.appmanager.pojo.App_Info;
+
+public interface App_infoService {
+	//修改APP信息
+		public int updateAppInfo(App_Info app_Info);
+		//查app信息byID
+	public App_Info appInfoById(Integer id);
 }
